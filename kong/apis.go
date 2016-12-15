3c9fb7b39ca08db9a093d7b8b4b69b9207191e78
Nick Curry <nicholas.curry@asurion.com>
2016-12-15 12:10:09 -0600

    Computer crashed so now I'm paranoid about saving often...

--- kong/apis.go
+++ kong/apis.go
@@ -1,5 +1,10 @@
 package kong
 
+import (
+	"net/http"
+	"fmt"
+)
+
 
 // ApisService handles communication with the apis resource of Kong
 type ApisService service
@@ -12,5 +17,46 @@ type Apis struct {
 }
 
 type Api struct {
+	UpstreamURL      string `json:"upstream_url"`
+	StripRequestPath bool   `json:"strip_request_path"`
+	RequestPath      string `json:"request_path"`
+	ID               string `json:"id"`
+	CreatedAt        int64  `json:"created_at"`
+	PreserveHost     bool   `json:"preserve_host"`
+	Name             string `json:"name"`
+}
+
+func (s *ApisService) Get(apiName string) (*Api, *http.Response, error) {
+	u := fmt.Sprintf("apis/%v", apiName)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	uResp := new(Api)
+	resp, err := s.client.Do(req, uResp)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return uResp, resp, err
+}
+
+func (s *ApisService) Patch(api *Api) (*http.Response, error) {
+	u := fmt.Sprintf("apis/%v", api.Name)
+
+	req, err := s.client.NewRequest("PATCH", u, api)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.client.Do(req, nil)
+
+	return resp, err
+
+}
+
+func (s *ApisService) GetAll(opt *ApisGetAllOptions) (*Apis, *http.Response, error) {
 
 }
--- kong/kong.go
+++ kong/kong.go
@@ -3,6 +3,9 @@ package kong
 import (
 	"net/http"
 	"net/url"
+	"io"
+	"bytes"
+	"encoding/json"
 )
 
 const (
@@ -28,4 +31,52 @@ type Client struct {
 
 type service struct {
 	client *Client
+}
+
+func NewClient(httpClient *http.Client, baseURLStr string) (*Client, err) {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	baseURL, err := url.Parse(baseURLStr)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Client{client: httpClient, BaseURL: baseURL}
+	c.common.client = c
+	c.Apis = (*ApisService)(&c.common)
+
+	return c, nil
+}
+
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	return req, nil
 }
